7af4637debfc8a2e4169368831b540bdd226e33c
Aisha <145033022+Aisha-syed9@users.noreply.github.com>
2025-01-18 18:42:18 +0400

    Delete Frontend directory

--- Frontend/login.js
+++
@@ -1,8 +0,0 @@
-// Navigation functionality for buttons
-function navigateTo(page) {
-    if (page === 'signin') {
-        window.location.href = 'signin.html'; // Replace with your sign-in page URL
-    } else if (page === 'login') {
-        window.location.href = 'login.html'; // Replace with your login page URL
-    }
-}
